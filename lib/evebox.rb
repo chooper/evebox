656731aa51aff4067b8ea48bb3193709c6de828d
Charles Hooper <charles@stripe.com>
2016-01-15 19:15:21 -0800

    Break env-breaking stuff to sep file

--- lib/evebox.rb
+++ lib/evebox.rb
@@ -1,33 +1,5 @@
 require "eaal"
-
-# Monkey patch
-ENV.instance_eval do
-  def source(filename)
-    return {} unless File.exists?(filename)
-
-    env = File.read(filename).split("\n").inject({}) do |hash, line|
-      if line =~ /\A([A-Za-z_0-9]+)=(.*)\z/
-        key, val = [$1, $2]
-        case val
-          when /\A'(.*)'\z/ then hash[key] = $1
-          when /\A"(.*)"\z/ then hash[key] = $1.gsub(/\\(.)/, '\1')
-          else hash[key] = val
-        end
-      end
-      hash
-    end
-
-    env.each { |k,v| ENV[k] = v unless ENV[k] }
-  end
-end
-
-def save_env!
-  File.open(".env", "w") do |f|
-    f.puts "EVE_KEY_ID=#{ENV['EVE_KEY_ID']}"
-    f.puts "EVE_TOKEN=#{ENV['EVE_TOKEN']}"
-    f.close
-  end
-end
+require_relative "env"
 
 def setup_tokens
   ENV.source(".env")
